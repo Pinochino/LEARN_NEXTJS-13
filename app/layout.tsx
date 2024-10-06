018bea24c301a3ec0ab29d3492bb9ca19f055316
Pinochino <tranhunghp22112004@gmail.com>
2024-10-06 08:50:55 +0700

    #17. Dàn Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Header Part</header>
         {children}
+        <footer>Footer Part</footer>
       </body>
     </html>
   );
